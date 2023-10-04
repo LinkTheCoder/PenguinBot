40998dc7b35df5e0603f78a6261b6e94b03ddee8
LinkTheCoder <66062268+LinkTheCoder@users.noreply.github.com>
2023-10-04 16:05:31 +0200

    update

---
+++ commands/skills/quotes.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+// Define an array of quotes and an array of image file paths
+const quotes = [
+    'Quote 1',
+    'Quote 2',
+    'Quote 3',
+    // Add more quotes as needed
+];
+
+const imagePaths = [
+    './img/quoetes/MorganaQuote.png',
+    './img/quoetes/RyujiQuote.png',
+    './img/quoetes/FutabaQuote.png',
+    // Add more image paths as needed
+];
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('quotes')
+        .setDescription('Replies with random quotes!'),
+    async execute(interaction) {
+        // Select a random quote and image path
+        const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        const randomImagePath = imagePaths[Math.floor(Math.random() * imagePaths.length)];
+
+        // Send a message with the random quote and image attachment
+        await interaction.reply({
+            content: randomQuote,
+            files: [
+                {
+                    attachment: randomImagePath,
+                    name: 'RandomImage.png', // You can set a fixed name for the image
+                },
+            ],
+        });
+    },
+};
