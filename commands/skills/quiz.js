54aa78bc6d534130bcb134253bf42d53a4017965
LinkTheCoder <66062268+LinkTheCoder@users.noreply.github.com>
2023-10-05 11:26:27 +0200

    update

---
+++ commands/skills/quiz.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+// Define an array of quiz pairs, where each pair contains two image paths
+const quizPairs = [
+    ['./img/quiz/MsUsamiSilverRatio.png', './img/quiz/SilverRatio.png'],
+    ['./img/quiz/MrInuiYoshitsune.png', './img/quiz/Yoshitsune.png'],
+    // Add more quiz pairs here as needed
+];
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('quiz')
+        .setDescription('Replies with a random school quiz with an image and an embed!'),
+    async execute(interaction) {
+        // Select a random quiz pair
+        const randomQuizIndex = Math.floor(Math.random() * quizPairs.length);
+        const randomQuiz = quizPairs[randomQuizIndex];
+
+        // Create an embed with the second image
+        const embed = {
+            title: 'Quiz',
+            image: {
+                url: `attachment://${randomQuiz[1]}`,
+            },
+        };
+
+        // Send the first image as an attachment and the embed as a response
+        await interaction.reply({
+            files: [
+                {
+                    attachment: randomQuiz[0],
+                    name: 'Image1.png',
+                },
+            ],
+            embeds: [embed],
+        });
+    },
+};
